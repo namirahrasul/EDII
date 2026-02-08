792b275805075d8b46629f6ccf48f2a0d9bd3935
namirahrasul <n.rsl.136@gmail.com>
2026-02-08 17:01:44 +0600

    commit

--- inpaint.sh
+++ inpaint.sh
@@ -9,5 +9,5 @@ order="3"
 method="singlestep"
 workdir="experiments/"$data"/"$steps_coarse"_"$steps"_"$DIS"_type-"$type
 
-CUDA_VISIBLE_DEVICES=0 python main.py   --config $data".yml"   --exp=$workdir   --sample   --timesteps=$steps   --timesteps_coarse=$steps_coarse   --eta 0   --ni   --skip_type=$DIS   --sample_type=$sampleMethod   --dpm_solver_order=$order   --dpm_solver_method=$method   --dpm_solver_type=$type   --base_samples ./demo_split/imagenet/val   --mask_path ./demo_split/mask/thick   --use_inverse_masks False   --image_folder results/celeba/thick   --seed 1234   --port 12355
+CUDA_VISIBLE_DEVICES=0 python main.py   --config $data".yml"   --exp=$workdir   --sample   --timesteps=$steps   --timesteps_coarse=$steps_coarse   --eta 0   --ni   --skip_type=$DIS   --sample_type=$sampleMethod   --dpm_solver_order=$order   --dpm_solver_method=$method   --dpm_solver_type=$type   --base_samples ./demo/image   --mask_path ./demo/mask   --use_inverse_masks False   --image_folder results/celeba   --seed 1234   --port 12355
 
